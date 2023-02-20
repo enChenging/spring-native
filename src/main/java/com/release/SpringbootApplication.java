ad82f3b4603d21b81c084fee2b24a5f16246cfb4
延成 <cyc.release@gmail.com>
2023-02-20 16:39:39 +0800

    Initial commit

---
+++ src/main/java/com/release/SpringbootApplication.java
@@ -0,0 +1,17 @@
+package com.release;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yancheng
+ * @since 2022/7/8
+ */
+@SpringBootApplication
+public class SpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApplication.class, args);
+    }
+
+}
